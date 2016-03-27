3329dcb3c48e8bf940468e4ff2a33fdbd57a59dd
Alex Plescan <alexpls@gmail.com>
2016-03-27 12:03:50 +1000

    Loading in audio and basic playback

--- rubin.js
+++ rubin.js
@@ -1,9 +1,12 @@
 import Sound from './lib/Sound'
 
+window.AudioContext = window.AudioContext || window.webkitAudioContext
+
 class Rubin {
   constructor() {
     this.sounds = {}
     this.loadQueue = []
+    this.context = new window.AudioContext()
   }
 
   preloadSounds(soundConfigs) {
@@ -15,6 +18,8 @@ class Rubin {
         console.log(`${soundsConfig.key} was already added, not adding it again`)
       }
     })
+
+    this.startLoadingFromQueue()
   }
 
   getSound(key, state) {
@@ -31,9 +36,43 @@ class Rubin {
   }
 
   addSound(soundConfig) {
-    const s = new Sound(soundConfig)
+    const s = new Sound(this.context, soundConfig)
     this.sounds[soundConfig.key] = s
     return s
+  }
+
+  startLoadingFromQueue() {
+    if (this.loadQueue.length === 0) { return; }
+
+    // how many sounds to load at one time?
+    const asyncLoadCount = 2
+
+    var startLoadingSound = (sound) => {
+      sound.load().then(function() {
+        // remove the sound from the load queue once it's ready to be played
+        const soundIdx = this.loadQueue.indexOf(sound)
+        this.loadQueue.splice(soundIdx, 1)
+
+        // checks if we've loaded all the sounds already
+        if (this.loadQueue.length === 0) {
+          return
+        }
+
+        // find the next sound that's not loading in the queue and
+        // start loading it!
+        this.loadQueue.some(function(soundInQueue) {
+          if (soundInQueue.state.is('initialized')) {
+            startLoadingSound(soundInQueue)
+            return true
+          }
+        })
+      })
+    }
+
+    // kick off the load
+    this.loadQueue.slice(0, asyncLoadCount).forEach((sound) => {
+      startLoadingSound(sound)
+    })
   }
 }
 
